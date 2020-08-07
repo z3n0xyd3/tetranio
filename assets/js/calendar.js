02b2413e06080e52226721a0854c2e8da2a0e827
Leandro <lrugna@opensolutions.com.ar>
2020-08-06 21:18:11 -0300

    Hola

---
+++ assets/js/calendar.js
@@ -0,0 +1,120 @@
+function generate_year_range(start, end) {
+  var years = "";
+  for (var year = start; year <= end; year++) {
+      years += "<option value='" + year + "'>" + year + "</option>";
+  }
+  return years;
+}
+
+var today = new Date();
+var nuestraFecha = new Date("Sat Oct 19 2019 19:48:29 GMT-0300 (Argentina Standard Time)");
+var currentMonth = today.getMonth();
+var currentYear = today.getFullYear();
+var selectYear = document.getElementById("year");
+var selectMonth = document.getElementById("month");
+
+
+var createYear = generate_year_range(1970, 2050);
+/** or
+* createYear = generate_year_range( 1970, currentYear );
+*/
+
+document.getElementById("year").innerHTML = createYear;
+
+var calendar = document.getElementById("calendar");
+var lang = calendar.getAttribute('data-lang');
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var days = ["dom", "lun", "mar", "mie", "jue", "vie", "sab"];
+
+var dayHeader = "<tr>";
+for (day in days) {
+  dayHeader += "<th data-days='" + days[day] + "'>" + days[day] + "</th>";
+}
+dayHeader += "</tr>";
+
+document.getElementById("thead-month").innerHTML = dayHeader;
+
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+
+function next() {
+  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+  currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+  var firstDay = ( new Date( year, month ) ).getDay();
+
+  tbl = document.getElementById("calendar-body");
+
+  
+  tbl.innerHTML = "";
+
+  
+  monthAndYear.innerHTML = months[month] + " " + year;
+  selectYear.value = year;
+  selectMonth.value = month;
+
+  // creating all cells
+  var date = 1;
+  for ( var i = 0; i < 6; i++ ) {
+      var row = document.createElement("tr");
+
+      for ( var j = 0; j < 7; j++ ) {
+          if ( i === 0 && j < firstDay ) {
+              cell = document.createElement( "td" );
+              cellText = document.createTextNode("");
+              cell.appendChild(cellText);
+              row.appendChild(cell);
+          } else if (date > daysInMonth(month, year)) {
+              break;
+          } else {
+              cell = document.createElement("td");
+			  cell.setAttribute("cursor", "pointer");
+              cell.setAttribute("data-date", date);
+              cell.setAttribute("data-month", month + 1);
+              cell.setAttribute("data-year", year);
+              cell.setAttribute("data-month_name", months[month]);
+              cell.className = "date-picker";
+              cell.innerHTML = "<span style='cursor: pointer'>" + date + "</span>";
+
+              if ( date === nuestraFecha.getDate() && year === nuestraFecha.getFullYear() && month === nuestraFecha.getMonth() ) {
+                  cell.innerHTML = "<span><a style='color: #000000' href='index.html'>"  + date + "</a></span>";
+              }
+
+              if ( date === today.getDate() && year === today.getFullYear() && month === today.getMonth() ) {
+                  cell.className = "date-picker selected";
+              }
+              row.appendChild(cell);
+              date++;
+          }
+
+
+      }
+
+      tbl.appendChild(row);
+  }
+
+}
+
+function daysInMonth(iMonth, iYear) {
+  return 32 - new Date(iYear, iMonth, 32).getDate();
+}
